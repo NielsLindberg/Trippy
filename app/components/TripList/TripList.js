2c186b404c586921149dd9a05ecab8c6ff9cb9b0
NielsLindberg <eighthourcream@gmail.com>
2017-03-17 13:15:24 +0100

    added input for place search webservice

--- app/components/TripList/TripList.js
+++ app/components/TripList/TripList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import {AppRegistry, Text, View, ScrollView, StyleSheet, TextInput, Switch, Button, TouchableOpacity} from 'react-native';
 import Backend from '../../modules/Backend/Backend';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -7,16 +7,58 @@ export default class TripList extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-		};
+			text: '',
+			response: ''
+		}
+		this.onSubmit = this.onSubmit.bind(this);
+		this.onTextChange = this.onTextChange.bind(this);
+
+		this.getPlacesSearch = this.getPlacesSearch.bind(this);
+		this.webServicePlaceSearch = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
+		this.apiKey = '&key=AIzaSyA7a8aISd9T8geGAmFiQbag2H_tf2XQK8A'
+	}
+	async getPlacesSearch() {
+    try {
+      let response = await fetch(this.webServicePlaceSearch + this.state.text + this.apiKey);
+      this.setState({
+            response: JSON.stringify(response)
+        });
+    } catch(error) {
+      this.setState({
+            response: JSON.stringify(error)
+        });
+    }
+  }
+	onTextChange(text){
+		this.setState({text});
+	}
+
+	onSubmit(){
+		this.getPlacesSearch();
 	}
 
 	render(){
-		return (
-	    <View style={styles.container}>
-	        <View style={styles.wrapper}>
-	        </View>
-	    </View>
-    );
+		return(
+			<ScrollView style={styles.container}>
+				<View style={styles.wrapper}>
+					<TextInput
+						value={this.state.text}
+						placeholder='SearchString'
+						onChangeText= {(value) => this.onTextChange(value)}
+					/>
+					<View>
+						<Button
+							title='SUBMIT'
+							onPress={this.onSubmit}
+							color='black'
+						/>
+					</View>
+					<View>
+						<Text>{this.state.response}</Text>
+					</View>
+				</View>
+			</ScrollView>
+		)
 	}
 }
 
